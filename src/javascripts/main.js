07ad145c51f00de1ec1af63e75a4f89badbf0861
nosemasashi1013 <nose_masashi@cyberagent.co.jp>
2022-04-08 12:02:34 +0900

    feat: アンカーリンク追加

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -154,6 +154,22 @@ $(function () {
 		});
 	});
 
+	$('a[href^="#"]').on("click", function () {
+		$(".anchor-nav-list__item").css("border-bottom", "2px solid transparent");
+		$(this)
+			.parent(".anchor-nav-list__item")
+			.css("border-bottom", "2px solid #008660");
+
+		const adjust = 0;
+		const speed = 400;
+		var href = $(this).attr("href");
+		const target = $(href === "#" || href === "" ? "html" : href);
+		const position = target.offset().top + adjust;
+		$("body,html").animate({ scrollTop: position }, speed, "swing");
+		return false;
+	});
+	$(".anchor-nav-list__item:first").css("border-bottom", "2px solid #008660");
+
 	dom.i2svg();
 });
 
