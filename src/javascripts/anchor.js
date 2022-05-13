577d4592dfd4dff043e622f5a02d6cde970bf3b5
nosemasashi <nose_masashi@cyberagent.co.jp>
2022-05-13 17:11:15 +0900

    fix: detail修正

--- src/javascripts/anchor.js
+++ src/javascripts/anchor.js
@@ -3,11 +3,6 @@
  */
 export const detailPageAnchor = () => {
 	$('a[href^="#"]').on("click", function () {
-		$(".anchor-nav-list__item").css("border-bottom", "2px solid transparent");
-		$(this)
-			.parent(".anchor-nav-list__item")
-			.css("border-bottom", "2px solid #008660");
-
 		const adjust = 0;
 		const speed = 400;
 		const href = $(this).attr("href");
@@ -16,5 +11,4 @@ export const detailPageAnchor = () => {
 		$("body,html").animate({ scrollTop: position }, speed, "swing");
 		return false;
 	});
-	$(".anchor-nav-list__item:first").css("border-bottom", "2px solid #008660");
 };
