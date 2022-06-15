bc1d3764d83c2851aff02e21a38721acf41adfe9
nosemasashi <nose_masashi@cyberagent.co.jp>
2022-06-15 13:55:32 +0900

    fix: 検索のヒント修正

--- src/javascripts/accordion.js
+++ src/javascripts/accordion.js
@@ -9,12 +9,14 @@ export const changeAccordionStatus = () => {
 				.parents(".accordion__list")
 				.find(".accordion-panel")
 				.attr("aria-hidden", "true");
+			$(this).parents(".accordion__header").css("border-radius", "8px");
 		} else {
 			$(this).attr("aria-expanded", "true");
 			$(this)
 				.parents(".accordion__list")
 				.find(".accordion-panel")
 				.attr("aria-hidden", "false");
+			$(this).parents(".accordion__header").css("border-radius", "8px 8px 0 0");
 		}
 	});
 };
