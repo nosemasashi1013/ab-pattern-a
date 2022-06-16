c91d75490edf15d64ca5a4f1d1f447299375cd41
nosemasashi <nose_masashi@cyberagent.co.jp>
2022-06-16 19:02:00 +0900

    feat: TOPリンクボタン設置

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,6 +4,7 @@ import {
 	faCheck,
 	faAngleRight,
 	faAngleDown,
+	faAngleUp,
 	faSearch,
 	faExclamationCircle,
 	faArrowLeft,
@@ -32,12 +33,15 @@ import { moveFocus } from "./header";
 import { createPagination } from "./pagination";
 import { categoryFilter } from "./filter";
 import { incrementalSearch } from "./incrementalSearch";
+import { showScrollTop } from "./scrollTop";
+import { scrollTop } from "./scrollTop";
 
 $(function () {
 	library.add(
 		faCheck,
 		faAngleRight,
 		faAngleDown,
+		faAngleUp,
 		faSearch,
 		faExclamationCircle,
 		faArrowLeft,
@@ -69,5 +73,7 @@ $(function () {
 	createPagination();
 	categoryFilter();
 	incrementalSearch();
+	showScrollTop();
+	scrollTop();
 	dom.i2svg();
 });
