b9d63a329c05f18b26f47507107da6e10c074fa3
nosemasashi <nose_masashi@cyberagent.co.jp>
2022-06-20 17:30:15 +0900

    feat:created pattern A

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -24,25 +24,6 @@ import {
 	faComments,
 } from "@fortawesome/free-regular-svg-icons";
 import "@accessible360/accessible-slick";
-import { initializeSlick, changeSlideOnHover } from "./slider";
-import { changeAccordionStatus } from "./accordion";
-import { showAlert } from "./alert";
-import { changeTabStatus } from "./tab";
-import { setFontColor, setFontSize, selectLanguage } from "./accessibility";
-import { detailPageAnchor } from "./anchor";
-import { getCurrentUrl } from "./globalNavi";
-import { toggleMenu } from "./header";
-import { moveFocus } from "./header";
-import { createPagination } from "./pagination";
-import { categoryFilter } from "./filter";
-import {
-	incrementalSearch,
-	faqSearch,
-	clickTipsList,
-} from "./incrementalSearch";
-import { showScrollTop } from "./scrollTop";
-import { scrollTop } from "./scrollTop";
-import { keywordSearch } from "./keywordSearch";
 
 $(function () {
 	library.add(
@@ -67,25 +48,5 @@ $(function () {
 		faComments
 	);
 
-	setFontColor();
-	setFontSize();
-	selectLanguage();
-	changeAccordionStatus();
-	changeTabStatus();
-	detailPageAnchor();
-	showAlert();
-	initializeSlick();
-	changeSlideOnHover();
-	getCurrentUrl();
-	toggleMenu();
-	moveFocus();
-	createPagination();
-	categoryFilter();
-	incrementalSearch();
-	showScrollTop();
-	scrollTop();
-	keywordSearch();
-	faqSearch();
-	clickTipsList();
 	dom.i2svg();
 });
